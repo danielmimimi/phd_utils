ea0031fe531093540d72b356f18f426f2a182e22
danielmimimi <daniel.klauser@outlook.com>
2024-06-13 07:44:40 +0200

    update

--- dataset_handler/delete_non_visible_series.py
+++ dataset_handler/delete_non_visible_series.py
@@ -1,45 +1,37 @@
-import argparse
-import os
-from pathlib import Path
-
 import cv2
-from tqdm import tqdm
-
+from pathlib import Path
 from annotation_handler_hslu import AnnotationHandlerHslu
 
 
-parser = argparse.ArgumentParser(
-                    prog='ProgramName',
-                    description='What the program does',
-                    epilog='Text at the bottom of help')
+def _find_longest_sequence(numbers):
+    # Find the longest sequence of consecutive numbers
+    longest_sequence = []
+    current_sequence = []
 
-parser.add_argument('-c','--config_path',default=r"D:\generated_annotated") 
-args = parser.parse_args()
+    for i in range(len(numbers)):
+        if i == 0 or numbers[i] == numbers[i - 1] + 1:
+            current_sequence.append(numbers[i])
+        else:
+            if len(current_sequence) > len(longest_sequence):
+                longest_sequence = current_sequence
+            current_sequence = [numbers[i]]
+    # Check the last sequence
+    if len(current_sequence) > len(longest_sequence):
+        longest_sequence = current_sequence
+    return longest_sequence
 
-keypoints_to_inspect = ["head_end","foot_l","foot_r"]
+def get_start_and_stop_frame(config_path:Path):
 
-print("Loaded Config in PostProcessing")
-args_config_path = Path(args.config_path)
-if not args_config_path.exists():
-    raise Exception("Not Exists {}".format(args.config_path))
-dataset_folders = os.listdir(args_config_path.as_posix())
+    keypoints_to_inspect = ["head_end","foot_l","foot_r"]
 
-unvalid = 0
-for folder in tqdm(dataset_folders):
-    image_path = args_config_path.joinpath(folder)
-
-    if not image_path.joinpath("annotation.csv").exists():
-        print("{} does not contain an annotation.csv".format(image_path.as_posix()))
-        continue
-    # READ ANNOTATION:CSV
-    new_annotation_handler = AnnotationHandlerHslu()
-
+    # READ ANNOTATION.CSV
     annotation_handler = AnnotationHandlerHslu()
-    annotation_handler.readAnnotations(image_path.joinpath("annotation.csv").as_posix())
+    annotation_handler.readAnnotations(config_path.joinpath("annotation.csv").as_posix())
     annotations = annotation_handler.getAnnotationItemsList()
     image_size_of_series = None
 
     not_valid_indexes = []
+    valid_indexes = []
     for index,annotation in enumerate(annotations):
         if image_size_of_series == None:
             correct_path = None
@@ -47,8 +39,8 @@ for folder in tqdm(dataset_folders):
             if Path(annotation.imageName.strip(":rot0")).exists():
                 correct_path = Path(annotation.imageName.strip(":rot0")).name
             else:
-                 correct_path = annotation.imageName.strip(":rot0")
-            img = cv2.imread(image_path.joinpath(correct_path).as_posix())
+                correct_path = annotation.imageName.strip(":rot0")
+            img = cv2.imread(config_path.joinpath(correct_path).as_posix())
             image_size_of_series = img.shape[0:2]
 
         # CHECK VISIBILITY OF PERSON IN IMAGE - SELECT FEET AND HEAD TIP
@@ -57,13 +49,22 @@ for folder in tqdm(dataset_folders):
             y = annotation.point[1]
             if x < 0 or y < 0 or y > image_size_of_series[0] or x > image_size_of_series[1]:
                 not_valid_indexes.append(annotation.imageName)
-                continue
+            else:
+                valid_indexes.append(annotation.imageName)
+         
         pass
-    if len(not_valid_indexes) > 0:
-        unvalid = unvalid + 1
+    unique_valid_indexes = set(valid_indexes)
+    unique_not_valid_indexes = set(not_valid_indexes)
+    unique_valid_indexes.difference_update(unique_not_valid_indexes)
+    numbers = sorted(int(item.split('_')[1].split('.')[0]) for item in unique_valid_indexes)
+    longest_sequence = _find_longest_sequence(numbers)
 
-    print(unvalid)
-pass
+    valid = True
+    if len(longest_sequence) < 8:
+        valid = False
+        return 0, 0,valid
+    return longest_sequence[0], longest_sequence[-1],valid
+
     
     # UPDATE ANNOTATION 
     # WRITE ANNOTATION
--- dataset_handler/generate_ccisn_dataset.py
+++ dataset_handler/generate_ccisn_dataset.py
@@ -1,8 +1,116 @@
 import argparse
-from dataset_handler.annotation_handler_hslu import AnnotationHandlerHslu
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', help='Path to config file', default='D:\generated')
-args = parser.parse_args()
+from tqdm import tqdm
+from annotation_handler_hslu import AnnotationHandlerHslu
 
-AnnotationHandlerHslu()
+
+import glob
+from pathlib import Path
+import os
+import json
+import random
+from sklearn.model_selection import train_test_split
+
+from delete_non_visible_series import get_start_and_stop_frame
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', help='Path to config file', default='D:\gen2', type=Path)
+    args = parser.parse_args()
+
+
+    local_paths = "/."
+
+
+    # args.dataset = Path("D:\gen2")
+
+    paths = [{"back_to_front":1},
+            {"front_to_back":0},
+            {"left_to_right":2},
+            {"right_to_left":3},
+            {"standing":4}]
+
+    data = []
+
+    min_samples = float('inf')
+    label_to_folders = {}
+
+    for path in tqdm(paths):
+        for name, label in path.items():
+            folders_with_data= glob.glob(args.dataset.joinpath(name+"_*").as_posix())
+            if label not in label_to_folders:
+                label_to_folders[label] = []
+
+            folders_to_remove = []
+
+            inner_tqdm = tqdm(enumerate(folders_with_data), total=len(folders_with_data), desc=f"Processing folders for {name}", leave=False)
+
+            for folder_index,folder_name in inner_tqdm:
+                annotations_file = Path(folder_name).joinpath("annotation.csv")
+                if annotations_file.exists():
+                    start,stop,valid = get_start_and_stop_frame(Path(folder_name))
+                    if not valid:
+                        folders_to_remove.append(folder_index)           
+                else:
+                    folders_to_remove.append(folder_index)
+
+            for index in sorted(folders_to_remove, reverse=True):
+                del folders_with_data[index]
+
+            label_to_folders[label].append((name, folders_with_data))
+            min_samples = min(min_samples, len(folders_with_data))
+
+
+    # Step 2: Limit the number of samples taken from each folder to the minimum count
+    for label, folders_list in label_to_folders.items():
+        for name, folders_with_data in folders_list:
+            limited_folders = folders_with_data[:min_samples]
+            for folder in limited_folders:
+                path_to_inspect = Path(folder)
+                start,stop,valid = get_start_and_stop_frame(path_to_inspect)
+                if valid:
+                    data.append({"id": path_to_inspect.name,
+                                "label": label,
+                                "start":start,
+                                "stop":stop})
+
+    # Shuffle the data to ensure random split
+    random.shuffle(data)
+
+    # Separate the data by labels
+    label_to_data = {}
+    for item in data:
+        label = item['label']
+        if label not in label_to_data:
+            label_to_data[label] = []
+        label_to_data[label].append(item)
+
+    # Split data while maintaining the balance of labels
+    train_data, val_data, test_data = [], [], []
+
+    for label, items in label_to_data.items():
+        train_items, test_items = train_test_split(items, test_size=0.2, random_state=42, stratify=[item['label'] for item in items])
+        train_items, val_items = train_test_split(train_items, test_size=0.25, random_state=42, stratify=[item['label'] for item in train_items])  # 0.25 * 0.8 = 0.2
+        train_data.extend(train_items)
+        val_data.extend(val_items)
+        test_data.extend(test_items)
+
+    # Shuffle the splits again to ensure random order
+    random.shuffle(train_data)
+    random.shuffle(val_data)
+    random.shuffle(test_data)
+
+    # Save the data to JSON files
+    def save_to_json(data, filename):
+        with open(filename, 'w') as f:
+            json.dump({"Data": data}, f, indent=4)
+
+    save_to_json(train_data, args.dataset.joinpath('annotations_train.json'))
+    save_to_json(val_data, args.dataset.joinpath('annotations_validation.json'))
+    save_to_json(test_data, args.dataset.joinpath('annotations_test.json'))
+
+
+if __name__ == "__main__":
+    main()
